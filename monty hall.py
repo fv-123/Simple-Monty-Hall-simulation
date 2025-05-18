07563bb0c673ce3c3a0274e7781f87d914761cab
Nguyen Phu Vinh <nguyenvinh250504@gmail.com>
2025-05-18 22:12:18 +0700

    Update monty hall.py

--- monty hall.py
+++ monty hall.py
@@ -2,10 +2,10 @@ import random as rd
 
 
 def the_game():
-    trial = 1000
+    trial = 1000 # run the simulation 1000 times
     k_win, s_win = 0, 0
     for _ in range(trial):
-        doors = [1, 2, 3]
+        doors = [1, 2, 3] # a list with 3 doors
         win = rd.choice(doors)
 
         choice = rd.choice(doors)
@@ -17,14 +17,14 @@ def the_game():
             after_choose = [win, choice]
 
         keep_win = (choice == win)
-        if keep_win:
+        if keep_win: # if keeping the choice wins
             k_win += 1
 
         switch_choice = [d for d in after_choose if d != choice][0]
         switch_win = (switch_choice == win)
-        if switch_win:
+        if switch_win: # if switching the choice wins
             s_win += 1
-    return f"If you dont change, you win {k_win} times. If you change, you win {s_win} times"
+    return f"If you dont switch, you win {k_win} times. If you switch, you win {s_win} times"
 
 
 the_game()
